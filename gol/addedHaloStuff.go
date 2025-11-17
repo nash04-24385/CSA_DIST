0aaf301fd6650a57fb3d93b50ac1be1c41a93265
Nasywa  Sakimun <tt24385@bristol.ac.uk>
2025-11-17 01:21:14 +0000

    trying to check halo correctness

--- gol/addedHaloStuff.go
+++ gol/addedHaloStuff.go
@@ -1,15 +1,16 @@
 package gol
 
 type WorkerInitRequest struct {
-	Params     Params
-	StartY     int      // global start row this worker owns
-	EndY       int      // global end row (exclusive)
-	LocalWorld [][]byte // initial slice
-	AboveAddr  string   // "ip:8030" of worker above
-	BelowAddr  string   // "ip:8030" of worker below
-
+	Params        Params
+	StartY        int      // global start row this worker owns
+	EndY          int      // global end row (exclusive)
+	LocalWorld    [][]byte // initial slice
+	AboveAddr     string   // "ip:8030" of worker above
+	BelowAddr     string   // "ip:8030" of worker below
+	TopInitial    []byte   //new
+	BottomInitial []byte   //new
 }
 
 type HaloRow struct {
-	Row   []byte
+	Row []byte
 }
